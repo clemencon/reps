e71dc5f99bda568fbd7d56c9e0558f62e85b3167
Julien Clémençon <julien@clemencon.dev>
2026-01-13 14:32:49 +0100

    Add a deck

---
+++ tests/Deck.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "vitest";
+import { Card } from "../src/Card.js";
+import { Deck } from "../src/Deck.js";
+
+describe("Deck", () => {
+	test("contains cards", () => {
+		const card1 = new Card("Q1?", "A1");
+		const deck = new Deck(card1);
+		expect(deck.cards).toEqual([card1]);
+	});
+
+	test("can be iterated over for studying", () => {
+		const card1 = new Card("Q1?", "A1");
+		const card2 = new Card("Q2?", "A2");
+		const deck = new Deck(card1, card2);
+		expect([...deck]).toEqual([card1, card2]);
+	});
+
+	test("can be empty", () => {
+		const deck = new Deck();
+		expect([...deck]).toEqual([]);
+	});
+});
